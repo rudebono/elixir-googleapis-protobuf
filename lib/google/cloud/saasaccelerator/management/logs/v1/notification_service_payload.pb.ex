70f9eaed4bca3de8f27605a7e879d9f2de8923bb
Hany Rock <rudebono@gmail.com>
2021-07-06 00:25:58 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/cloud/saasaccelerator/management/logs/v1/notification_service_payload.pb.ex
@@ -0,0 +1,51 @@
+defmodule Google.Cloud.Saasaccelerator.Management.Logs.V1.NotificationStage.Stage do
+  @moduledoc false
+  use Protobuf, enum: true, syntax: :proto3
+  @type t :: integer | :STAGE_UNSPECIFIED | :SENT | :SEND_FAILURE | :DROPPED
+
+  field :STAGE_UNSPECIFIED, 0
+
+  field :SENT, 1
+
+  field :SEND_FAILURE, 2
+
+  field :DROPPED, 3
+end
+
+defmodule Google.Cloud.Saasaccelerator.Management.Logs.V1.NotificationStage.Event do
+  @moduledoc false
+  use Protobuf, enum: true, syntax: :proto3
+  @type t :: integer | :EVENT_UNSPECIFIED | :HEALTH_STATUS_CHANGE
+
+  field :EVENT_UNSPECIFIED, 0
+
+  field :HEALTH_STATUS_CHANGE, 1
+end
+
+defmodule Google.Cloud.Saasaccelerator.Management.Logs.V1.NotificationStage do
+  @moduledoc false
+  use Protobuf, syntax: :proto3
+
+  @type t :: %__MODULE__{
+          stage: Google.Cloud.Saasaccelerator.Management.Logs.V1.NotificationStage.Stage.t(),
+          event_time: Google.Protobuf.Timestamp.t() | nil,
+          notification_id: String.t(),
+          event: Google.Cloud.Saasaccelerator.Management.Logs.V1.NotificationStage.Event.t(),
+          message: String.t()
+        }
+
+  defstruct [:stage, :event_time, :notification_id, :event, :message]
+
+  field :stage, 1,
+    type: Google.Cloud.Saasaccelerator.Management.Logs.V1.NotificationStage.Stage,
+    enum: true
+
+  field :event_time, 2, type: Google.Protobuf.Timestamp
+  field :notification_id, 3, type: :string
+
+  field :event, 4,
+    type: Google.Cloud.Saasaccelerator.Management.Logs.V1.NotificationStage.Event,
+    enum: true
+
+  field :message, 5, type: :string
+end
