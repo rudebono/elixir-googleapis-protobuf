b46ef94ff2892e9adf9544394ece6522c5145490
Hany Rock <rudebono@gmail.com>
2022-05-27 00:31:42 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

--- lib/google/firestore/admin/v1/database.pb.ex
+++ lib/google/firestore/admin/v1/database.pb.ex
@@ -15,6 +15,14 @@ defmodule Google.Firestore.Admin.V1.Database.ConcurrencyMode do
   field :PESSIMISTIC, 2
   field :OPTIMISTIC_WITH_ENTITY_GROUPS, 3
 end
+defmodule Google.Firestore.Admin.V1.Database.AppEngineIntegrationMode do
+  @moduledoc false
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :APP_ENGINE_INTEGRATION_MODE_UNSPECIFIED, 0
+  field :ENABLED, 1
+  field :DISABLED, 2
+end
 defmodule Google.Firestore.Admin.V1.Database do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
@@ -28,5 +36,11 @@ defmodule Google.Firestore.Admin.V1.Database do
     json_name: "concurrencyMode",
     enum: true
 
+  field :app_engine_integration_mode, 19,
+    type: Google.Firestore.Admin.V1.Database.AppEngineIntegrationMode,
+    json_name: "appEngineIntegrationMode",
+    enum: true
+
+  field :key_prefix, 20, type: :string, json_name: "keyPrefix", deprecated: false
   field :etag, 99, type: :string
 end
--- lib/google/firestore/admin/v1/field.pb.ex
+++ lib/google/firestore/admin/v1/field.pb.ex
@@ -1,3 +1,12 @@
+defmodule Google.Firestore.Admin.V1.Field.TtlConfig.State do
+  @moduledoc false
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :STATE_UNSPECIFIED, 0
+  field :CREATING, 1
+  field :ACTIVE, 2
+  field :NEEDS_REPAIR, 3
+end
 defmodule Google.Firestore.Admin.V1.Field.IndexConfig do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
@@ -6,6 +15,15 @@ defmodule Google.Firestore.Admin.V1.Field.IndexConfig do
   field :uses_ancestor_config, 2, type: :bool, json_name: "usesAncestorConfig"
   field :ancestor_field, 3, type: :string, json_name: "ancestorField"
   field :reverting, 4, type: :bool
+end
+defmodule Google.Firestore.Admin.V1.Field.TtlConfig do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :state, 1,
+    type: Google.Firestore.Admin.V1.Field.TtlConfig.State,
+    enum: true,
+    deprecated: false
 end
 defmodule Google.Firestore.Admin.V1.Field do
   @moduledoc false
@@ -16,4 +34,6 @@ defmodule Google.Firestore.Admin.V1.Field do
   field :index_config, 2,
     type: Google.Firestore.Admin.V1.Field.IndexConfig,
     json_name: "indexConfig"
+
+  field :ttl_config, 3, type: Google.Firestore.Admin.V1.Field.TtlConfig, json_name: "ttlConfig"
 end
