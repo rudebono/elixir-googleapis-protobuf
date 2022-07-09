d2f11acad29a4b53539a3d3ffe0e807997b7ae79
Hany Rock <rudebono@gmail.com>
2022-07-09 00:28:05 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/cloud/commerce/consumer/procurement/v1alpha1/order.pb.ex
@@ -0,0 +1,174 @@
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemChangeType do
+  @moduledoc false
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :LINE_ITEM_CHANGE_TYPE_UNSPECIFIED, 0
+  field :LINE_ITEM_CHANGE_TYPE_CREATE, 1
+  field :LINE_ITEM_CHANGE_TYPE_UPDATE, 2
+  field :LINE_ITEM_CHANGE_TYPE_CANCEL, 3
+  field :LINE_ITEM_CHANGE_TYPE_REVERT_CANCELLATION, 4
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemChangeState do
+  @moduledoc false
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :LINE_ITEM_CHANGE_STATE_UNSPECIFIED, 0
+  field :LINE_ITEM_CHANGE_STATE_PENDING_APPROVAL, 1
+  field :LINE_ITEM_CHANGE_STATE_APPROVED, 2
+  field :LINE_ITEM_CHANGE_STATE_COMPLETED, 3
+  field :LINE_ITEM_CHANGE_STATE_REJECTED, 4
+  field :LINE_ITEM_CHANGE_STATE_ABANDONED, 5
+  field :LINE_ITEM_CHANGE_STATE_ACTIVATING, 6
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemChangeStateReasonType do
+  @moduledoc false
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :LINE_ITEM_CHANGE_STATE_REASON_TYPE_UNSPECIFIED, 0
+  field :LINE_ITEM_CHANGE_STATE_REASON_TYPE_EXPIRED, 1
+  field :LINE_ITEM_CHANGE_STATE_REASON_TYPE_USER_CANCELLED, 2
+  field :LINE_ITEM_CHANGE_STATE_REASON_TYPE_SYSTEM_CANCELLED, 3
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.Order do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :name, 1, type: :string, deprecated: false
+  field :display_name, 10, type: :string, json_name: "displayName", deprecated: false
+
+  field :line_items, 6,
+    repeated: true,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItem,
+    json_name: "lineItems",
+    deprecated: false
+
+  field :cancelled_line_items, 7,
+    repeated: true,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItem,
+    json_name: "cancelledLineItems",
+    deprecated: false
+
+  field :create_time, 8,
+    type: Google.Protobuf.Timestamp,
+    json_name: "createTime",
+    deprecated: false
+
+  field :update_time, 9,
+    type: Google.Protobuf.Timestamp,
+    json_name: "updateTime",
+    deprecated: false
+
+  field :etag, 11, type: :string
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItem do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :line_item_id, 1, type: :string, json_name: "lineItemId", deprecated: false
+
+  field :line_item_info, 2,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemInfo,
+    json_name: "lineItemInfo",
+    deprecated: false
+
+  field :pending_change, 3,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemChange,
+    json_name: "pendingChange",
+    deprecated: false
+
+  field :change_history, 4,
+    repeated: true,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemChange,
+    json_name: "changeHistory",
+    deprecated: false
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemChange do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :change_id, 1, type: :string, json_name: "changeId", deprecated: false
+
+  field :change_type, 2,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemChangeType,
+    json_name: "changeType",
+    enum: true,
+    deprecated: false
+
+  field :old_line_item_info, 3,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemInfo,
+    json_name: "oldLineItemInfo",
+    deprecated: false
+
+  field :new_line_item_info, 4,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemInfo,
+    json_name: "newLineItemInfo"
+
+  field :change_state, 5,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemChangeState,
+    json_name: "changeState",
+    enum: true,
+    deprecated: false
+
+  field :state_reason, 6, type: :string, json_name: "stateReason", deprecated: false
+
+  field :change_state_reason_type, 10,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemChangeStateReasonType,
+    json_name: "changeStateReasonType",
+    enum: true,
+    deprecated: false
+
+  field :change_effective_time, 7,
+    type: Google.Protobuf.Timestamp,
+    json_name: "changeEffectiveTime",
+    deprecated: false
+
+  field :create_time, 8,
+    type: Google.Protobuf.Timestamp,
+    json_name: "createTime",
+    deprecated: false
+
+  field :update_time, 9,
+    type: Google.Protobuf.Timestamp,
+    json_name: "updateTime",
+    deprecated: false
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemInfo do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :offer, 13, type: :string, deprecated: false
+
+  field :parameters, 9,
+    repeated: true,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.Parameter,
+    deprecated: false
+
+  field :subscription, 10,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.Subscription,
+    deprecated: false
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.Parameter.Value do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  oneof :kind, 0
+
+  field :int64_value, 3, type: :int64, json_name: "int64Value", oneof: 0
+  field :string_value, 4, type: :string, json_name: "stringValue", oneof: 0
+  field :double_value, 5, type: :double, json_name: "doubleValue", oneof: 0
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.Parameter do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :name, 1, type: :string
+  field :value, 2, type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.Parameter.Value
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.Subscription do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :start_time, 3, type: Google.Protobuf.Timestamp, json_name: "startTime"
+  field :end_time, 1, type: Google.Protobuf.Timestamp, json_name: "endTime"
+  field :auto_renewal_enabled, 2, type: :bool, json_name: "autoRenewalEnabled"
+end
---
+++ lib/google/cloud/commerce/consumer/procurement/v1alpha1/procurement_service.pb.ex
@@ -0,0 +1,69 @@
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.PlaceOrderRequest do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :parent, 1, type: :string, deprecated: false
+  field :display_name, 6, type: :string, json_name: "displayName", deprecated: false
+
+  field :line_item_info, 10,
+    repeated: true,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.LineItemInfo,
+    json_name: "lineItemInfo",
+    deprecated: false
+
+  field :request_id, 7, type: :string, json_name: "requestId", deprecated: false
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.PlaceOrderMetadata do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.GetOrderRequest do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :name, 1, type: :string, deprecated: false
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.ListOrdersRequest do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :parent, 1, type: :string, deprecated: false
+  field :page_size, 2, type: :int32, json_name: "pageSize"
+  field :page_token, 3, type: :string, json_name: "pageToken"
+  field :filter, 4, type: :string
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.ListOrdersResponse do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :orders, 1,
+    repeated: true,
+    type: Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.Order
+
+  field :next_page_token, 2, type: :string, json_name: "nextPageToken"
+end
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.ConsumerProcurementService.Service do
+  @moduledoc false
+  use GRPC.Service,
+    name: "google.cloud.commerce.consumer.procurement.v1alpha1.ConsumerProcurementService",
+    protoc_gen_elixir_version: "0.10.0"
+
+  rpc :PlaceOrder,
+      Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.PlaceOrderRequest,
+      Google.Longrunning.Operation
+
+  rpc :GetOrder,
+      Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.GetOrderRequest,
+      Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.Order
+
+  rpc :ListOrders,
+      Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.ListOrdersRequest,
+      Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.ListOrdersResponse
+end
+
+defmodule Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.ConsumerProcurementService.Stub do
+  @moduledoc false
+  use GRPC.Stub,
+    service:
+      Google.Cloud.Commerce.Consumer.Procurement.V1alpha1.ConsumerProcurementService.Service
+end
