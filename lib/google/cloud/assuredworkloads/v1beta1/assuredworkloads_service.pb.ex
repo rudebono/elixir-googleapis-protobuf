671fbbc24d8531e2b98e5289361f5b3c55f20f23
Hany Rock <rudebono@gmail.com>
2022-07-19 00:33:52 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/cloud/assuredworkloads/v1beta1/assuredworkloads_service.pb.ex
@@ -0,0 +1,43 @@
+defmodule Google.Cloud.Assuredworkloads.V1beta1.AssuredWorkloadsService.Service do
+  @moduledoc false
+  use GRPC.Service,
+    name: "google.cloud.assuredworkloads.v1beta1.AssuredWorkloadsService",
+    protoc_gen_elixir_version: "0.10.0"
+
+  rpc :CreateWorkload,
+      Google.Cloud.Assuredworkloads.V1beta1.CreateWorkloadRequest,
+      Google.Longrunning.Operation
+
+  rpc :UpdateWorkload,
+      Google.Cloud.Assuredworkloads.V1beta1.UpdateWorkloadRequest,
+      Google.Cloud.Assuredworkloads.V1beta1.Workload
+
+  rpc :RestrictAllowedServices,
+      Google.Cloud.Assuredworkloads.V1beta1.RestrictAllowedServicesRequest,
+      Google.Cloud.Assuredworkloads.V1beta1.RestrictAllowedServicesResponse
+
+  rpc :RestrictAllowedResources,
+      Google.Cloud.Assuredworkloads.V1beta1.RestrictAllowedResourcesRequest,
+      Google.Cloud.Assuredworkloads.V1beta1.RestrictAllowedResourcesResponse
+
+  rpc :DeleteWorkload,
+      Google.Cloud.Assuredworkloads.V1beta1.DeleteWorkloadRequest,
+      Google.Protobuf.Empty
+
+  rpc :GetWorkload,
+      Google.Cloud.Assuredworkloads.V1beta1.GetWorkloadRequest,
+      Google.Cloud.Assuredworkloads.V1beta1.Workload
+
+  rpc :AnalyzeWorkloadMove,
+      Google.Cloud.Assuredworkloads.V1beta1.AnalyzeWorkloadMoveRequest,
+      Google.Cloud.Assuredworkloads.V1beta1.AnalyzeWorkloadMoveResponse
+
+  rpc :ListWorkloads,
+      Google.Cloud.Assuredworkloads.V1beta1.ListWorkloadsRequest,
+      Google.Cloud.Assuredworkloads.V1beta1.ListWorkloadsResponse
+end
+
+defmodule Google.Cloud.Assuredworkloads.V1beta1.AssuredWorkloadsService.Stub do
+  @moduledoc false
+  use GRPC.Stub, service: Google.Cloud.Assuredworkloads.V1beta1.AssuredWorkloadsService.Service
+end
---
+++ lib/google/cloud/identitytoolkit/logging/request_log.pb.ex
@@ -0,0 +1,23 @@
+defmodule Google.Cloud.Identitytoolkit.Logging.RequestLog do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :method_name, 1, type: :string, json_name: "methodName"
+  field :status, 2, type: Google.Rpc.Status
+
+  field :request_metadata, 3,
+    type: Google.Cloud.Identitytoolkit.Logging.RequestMetadata,
+    json_name: "requestMetadata"
+
+  field :request, 4, type: Google.Protobuf.Struct
+  field :response, 5, type: Google.Protobuf.Struct
+  field :num_response_items, 6, type: :int64, json_name: "numResponseItems"
+  field :metadata, 7, type: Google.Protobuf.Struct
+end
+defmodule Google.Cloud.Identitytoolkit.Logging.RequestMetadata do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :caller_ip, 1, type: :string, json_name: "callerIp"
+  field :caller_supplied_user_agent, 2, type: :string, json_name: "callerSuppliedUserAgent"
+end
