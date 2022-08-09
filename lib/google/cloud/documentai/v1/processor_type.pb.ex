44f8fd91ea0ff7a06acbc40cc4f50246f045f7ad
Hany Rock <rudebono@gmail.com>
2022-08-09 00:28:11 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/cloud/documentai/v1/processor_type.pb.ex
@@ -0,0 +1,22 @@
+defmodule Google.Cloud.Documentai.V1.ProcessorType.LocationInfo do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :location_id, 1, type: :string, json_name: "locationId"
+end
+defmodule Google.Cloud.Documentai.V1.ProcessorType do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+
+  field :name, 1, type: :string
+  field :type, 2, type: :string
+  field :category, 3, type: :string
+
+  field :available_locations, 4,
+    repeated: true,
+    type: Google.Cloud.Documentai.V1.ProcessorType.LocationInfo,
+    json_name: "availableLocations"
+
+  field :allow_creation, 6, type: :bool, json_name: "allowCreation"
+  field :launch_stage, 8, type: Google.Api.LaunchStage, json_name: "launchStage", enum: true
+end
