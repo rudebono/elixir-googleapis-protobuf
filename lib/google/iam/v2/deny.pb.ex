7131e3ca24d6739fb4c7ce3f918d9b48a721e1f4
Hany Rock <rudebono@gmail.com>
2022-08-29 00:32:53 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/iam/v2/deny.pb.ex
@@ -0,0 +1,15 @@
+defmodule Google.Iam.V2.DenyRule do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field :denied_principals, 1, repeated: true, type: :string, json_name: "deniedPrincipals"
+  field :exception_principals, 2, repeated: true, type: :string, json_name: "exceptionPrincipals"
+  field :denied_permissions, 3, repeated: true, type: :string, json_name: "deniedPermissions"
+
+  field :exception_permissions, 4,
+    repeated: true,
+    type: :string,
+    json_name: "exceptionPermissions"
+
+  field :denial_condition, 5, type: Google.Type.Expr, json_name: "denialCondition"
+end
