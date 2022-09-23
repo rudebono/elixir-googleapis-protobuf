d105538ee3b1e7cb09c5c6db17c763f217c4f641
Hany Rock <rudebono@gmail.com>
2022-09-23 00:33:11 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/cloud/sensitiveaction/logging/v1/sensitive_action_payload.pb.ex
@@ -0,0 +1,26 @@
+defmodule Google.Cloud.Sensitiveaction.Logging.V1.SensitiveAction.SourceLogId do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field :resource_container, 1, type: :string, json_name: "resourceContainer"
+  field :log_time, 2, type: Google.Protobuf.Timestamp, json_name: "logTime"
+  field :insert_id, 3, type: :string, json_name: "insertId"
+  field :query_uri, 4, type: :string, json_name: "queryUri"
+end
+
+defmodule Google.Cloud.Sensitiveaction.Logging.V1.SensitiveAction do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field :action_type, 1, type: :string, json_name: "actionType"
+  field :action_time, 2, type: Google.Protobuf.Timestamp, json_name: "actionTime"
+  field :affected_resources, 3, repeated: true, type: :string, json_name: "affectedResources"
+
+  field :source_log_ids, 4,
+    repeated: true,
+    type: Google.Cloud.Sensitiveaction.Logging.V1.SensitiveAction.SourceLogId,
+    json_name: "sourceLogIds"
+
+  field :learn_more_uri, 5, type: :string, json_name: "learnMoreUri"
+  field :access, 6, type: Google.Cloud.Securitycenter.V1.Access
+end
