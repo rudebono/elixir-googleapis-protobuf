4fad9fb83e475855041dc988c140797bbe8a0fbd
Hany Rock <rudebono@gmail.com>
2023-01-09 00:29:48 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/monitoring/v3/snooze.pb.ex
@@ -0,0 +1,16 @@
+defmodule Google.Monitoring.V3.Snooze.Criteria do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field :policies, 1, repeated: true, type: :string, deprecated: false
+end
+
+defmodule Google.Monitoring.V3.Snooze do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field :name, 1, type: :string, deprecated: false
+  field :criteria, 3, type: Google.Monitoring.V3.Snooze.Criteria, deprecated: false
+  field :interval, 4, type: Google.Monitoring.V3.TimeInterval, deprecated: false
+  field :display_name, 5, type: :string, json_name: "displayName", deprecated: false
+end
