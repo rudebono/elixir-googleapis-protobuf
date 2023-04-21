bc95d9381db51f0817f6e096a1955400d0b25253
Hany Rock <rudebono@gmail.com>
2023-04-21 00:24:32 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/cloud/securitycenter/v1/cloud_dlp_data_profile.pb.ex
@@ -0,0 +1,6 @@
+defmodule Google.Cloud.Securitycenter.V1.CloudDlpDataProfile do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field :data_profile, 1, type: :string, json_name: "dataProfile", deprecated: false
+end
