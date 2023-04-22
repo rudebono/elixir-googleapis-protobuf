c875c56288a21f85001f3a8ea4d2681f9e5f9657
Hany Rock <rudebono@gmail.com>
2023-04-22 00:27:02 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

--- lib/google/cloud/discoveryengine/v1beta/document.pb.ex
+++ lib/google/cloud/discoveryengine/v1beta/document.pb.ex
@@ -1,3 +1,14 @@
+defmodule Google.Cloud.Discoveryengine.V1beta.Document.Content do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  oneof :content, 0
+
+  field :raw_bytes, 2, type: :bytes, json_name: "rawBytes", oneof: 0
+  field :uri, 3, type: :string, oneof: 0
+  field :mime_type, 1, type: :string, json_name: "mimeType"
+end
+
 defmodule Google.Cloud.Discoveryengine.V1beta.Document do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
@@ -9,5 +20,11 @@ defmodule Google.Cloud.Discoveryengine.V1beta.Document do
   field :name, 1, type: :string, deprecated: false
   field :id, 2, type: :string, deprecated: false
   field :schema_id, 3, type: :string, json_name: "schemaId"
+  field :content, 10, type: Google.Cloud.Discoveryengine.V1beta.Document.Content
   field :parent_document_id, 7, type: :string, json_name: "parentDocumentId"
+
+  field :derived_struct_data, 6,
+    type: Google.Protobuf.Struct,
+    json_name: "derivedStructData",
+    deprecated: false
 end
