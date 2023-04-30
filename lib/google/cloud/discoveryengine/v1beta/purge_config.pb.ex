9c1dcf04c582adce56904657bb0fd2f6478f9048
Hany Rock <rudebono@gmail.com>
2023-04-30 00:26:47 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/cloud/discoveryengine/v1beta/purge_config.pb.ex
@@ -0,0 +1,34 @@
+defmodule Google.Cloud.Discoveryengine.V1beta.PurgeDocumentsRequest do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :parent, 1, type: :string, deprecated: false
+  field :filter, 2, type: :string, deprecated: false
+  field :force, 3, type: :bool
+end
+
+defmodule Google.Cloud.Discoveryengine.V1beta.PurgeDocumentsResponse do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :purge_count, 1, type: :int64, json_name: "purgeCount"
+
+  field :purge_sample, 2,
+    repeated: true,
+    type: :string,
+    json_name: "purgeSample",
+    deprecated: false
+end
+
+defmodule Google.Cloud.Discoveryengine.V1beta.PurgeDocumentsMetadata do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :create_time, 1, type: Google.Protobuf.Timestamp, json_name: "createTime"
+  field :update_time, 2, type: Google.Protobuf.Timestamp, json_name: "updateTime"
+  field :success_count, 3, type: :int64, json_name: "successCount"
+  field :failure_count, 4, type: :int64, json_name: "failureCount"
+end
