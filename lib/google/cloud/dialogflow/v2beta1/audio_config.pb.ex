58b2311d2640c5e97164fd7a49f99098cbad2afa
Hany Rock <rudebono@gmail.com>
2023-07-10 00:30:38 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

--- lib/google/cloud/dialogflow/v2beta1/audio_config.pb.ex
+++ lib/google/cloud/dialogflow/v2beta1/audio_config.pb.ex
@@ -1,3 +1,27 @@
+defmodule Google.Cloud.Dialogflow.V2beta1.TelephonyDtmf do
+  @moduledoc false
+
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :TELEPHONY_DTMF_UNSPECIFIED, 0
+  field :DTMF_ONE, 1
+  field :DTMF_TWO, 2
+  field :DTMF_THREE, 3
+  field :DTMF_FOUR, 4
+  field :DTMF_FIVE, 5
+  field :DTMF_SIX, 6
+  field :DTMF_SEVEN, 7
+  field :DTMF_EIGHT, 8
+  field :DTMF_NINE, 9
+  field :DTMF_ZERO, 10
+  field :DTMF_A, 11
+  field :DTMF_B, 12
+  field :DTMF_C, 13
+  field :DTMF_D, 14
+  field :DTMF_STAR, 15
+  field :DTMF_POUND, 16
+end
+
 defmodule Google.Cloud.Dialogflow.V2beta1.AudioEncoding do
   @moduledoc false
 
@@ -48,37 +72,13 @@ defmodule Google.Cloud.Dialogflow.V2beta1.OutputAudioEncoding do
   field :OUTPUT_AUDIO_ENCODING_MULAW, 5
 end
 
-defmodule Google.Cloud.Dialogflow.V2beta1.TelephonyDtmf do
-  @moduledoc false
-
-  use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
-
-  field :TELEPHONY_DTMF_UNSPECIFIED, 0
-  field :DTMF_ONE, 1
-  field :DTMF_TWO, 2
-  field :DTMF_THREE, 3
-  field :DTMF_FOUR, 4
-  field :DTMF_FIVE, 5
-  field :DTMF_SIX, 6
-  field :DTMF_SEVEN, 7
-  field :DTMF_EIGHT, 8
-  field :DTMF_NINE, 9
-  field :DTMF_ZERO, 10
-  field :DTMF_A, 11
-  field :DTMF_B, 12
-  field :DTMF_C, 13
-  field :DTMF_D, 14
-  field :DTMF_STAR, 15
-  field :DTMF_POUND, 16
-end
-
 defmodule Google.Cloud.Dialogflow.V2beta1.SpeechContext do
   @moduledoc false
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :phrases, 1, repeated: true, type: :string
-  field :boost, 2, type: :float
+  field :phrases, 1, repeated: true, type: :string, deprecated: false
+  field :boost, 2, type: :float, deprecated: false
 end
 
 defmodule Google.Cloud.Dialogflow.V2beta1.SpeechWordInfo do
@@ -109,10 +109,11 @@ defmodule Google.Cloud.Dialogflow.V2beta1.InputAudioConfig do
   field :audio_encoding, 1,
     type: Google.Cloud.Dialogflow.V2beta1.AudioEncoding,
     json_name: "audioEncoding",
-    enum: true
+    enum: true,
+    deprecated: false
 
-  field :sample_rate_hertz, 2, type: :int32, json_name: "sampleRateHertz"
-  field :language_code, 3, type: :string, json_name: "languageCode"
+  field :sample_rate_hertz, 2, type: :int32, json_name: "sampleRateHertz", deprecated: false
+  field :language_code, 3, type: :string, json_name: "languageCode", deprecated: false
   field :enable_word_info, 13, type: :bool, json_name: "enableWordInfo"
 
   field :phrase_hints, 4,
@@ -151,12 +152,13 @@ defmodule Google.Cloud.Dialogflow.V2beta1.VoiceSelectionParams do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :name, 1, type: :string
+  field :name, 1, type: :string, deprecated: false
 
   field :ssml_gender, 2,
     type: Google.Cloud.Dialogflow.V2beta1.SsmlVoiceGender,
     json_name: "ssmlGender",
-    enum: true
+    enum: true,
+    deprecated: false
 end
 
 defmodule Google.Cloud.Dialogflow.V2beta1.SynthesizeSpeechConfig do
@@ -164,11 +166,17 @@ defmodule Google.Cloud.Dialogflow.V2beta1.SynthesizeSpeechConfig do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :speaking_rate, 1, type: :double, json_name: "speakingRate"
-  field :pitch, 2, type: :double
-  field :volume_gain_db, 3, type: :double, json_name: "volumeGainDb"
-  field :effects_profile_id, 5, repeated: true, type: :string, json_name: "effectsProfileId"
-  field :voice, 4, type: Google.Cloud.Dialogflow.V2beta1.VoiceSelectionParams
+  field :speaking_rate, 1, type: :double, json_name: "speakingRate", deprecated: false
+  field :pitch, 2, type: :double, deprecated: false
+  field :volume_gain_db, 3, type: :double, json_name: "volumeGainDb", deprecated: false
+
+  field :effects_profile_id, 5,
+    repeated: true,
+    type: :string,
+    json_name: "effectsProfileId",
+    deprecated: false
+
+  field :voice, 4, type: Google.Cloud.Dialogflow.V2beta1.VoiceSelectionParams, deprecated: false
 end
 
 defmodule Google.Cloud.Dialogflow.V2beta1.OutputAudioConfig do
