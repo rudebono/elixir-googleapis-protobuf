beb7e24a117c6957439b9153df16673876cee51d
Hany Rock <rudebono@gmail.com>
2023-09-15 00:29:00 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

--- lib/google/api/pb_extension.pb.ex
+++ lib/google/api/pb_extension.pb.ex
@@ -2,18 +2,7 @@ defmodule Google.Api.PbExtension do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.12.0"
 
-  extend Google.Protobuf.MethodOptions, :method_signature, 1051,
-    repeated: true,
-    type: :string,
-    json_name: "methodSignature"
-
-  extend Google.Protobuf.ServiceOptions, :default_host, 1049,
+  extend Google.Protobuf.MethodOptions, :http, 72_295_728,
     optional: true,
-    type: :string,
-    json_name: "defaultHost"
-
-  extend Google.Protobuf.ServiceOptions, :oauth_scopes, 1050,
-    optional: true,
-    type: :string,
-    json_name: "oauthScopes"
+    type: Google.Api.HttpRule
 end
