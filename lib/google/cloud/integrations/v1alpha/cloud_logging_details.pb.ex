e7d8bad42565f74fe3f468c9467a8d6b99b8818c
Hany Rock <rudebono@gmail.com>
2024-02-01 00:27:41 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/cloud/integrations/v1alpha/cloud_logging_details.pb.ex
@@ -0,0 +1,29 @@
+defmodule Google.Cloud.Integrations.V1alpha.CloudLoggingDetails.CloudLoggingSeverity do
+  @moduledoc false
+
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :CLOUD_LOGGING_SEVERITY_UNSPECIFIED, 0
+  field :INFO, 2
+  field :ERROR, 3
+  field :WARNING, 4
+end
+
+defmodule Google.Cloud.Integrations.V1alpha.CloudLoggingDetails do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :cloud_logging_severity, 1,
+    proto3_optional: true,
+    type: Google.Cloud.Integrations.V1alpha.CloudLoggingDetails.CloudLoggingSeverity,
+    json_name: "cloudLoggingSeverity",
+    enum: true,
+    deprecated: false
+
+  field :enable_cloud_logging, 2,
+    proto3_optional: true,
+    type: :bool,
+    json_name: "enableCloudLogging",
+    deprecated: false
+end
---
+++ lib/google/cloud/integrations/v1alpha/integration_state.pb.ex
@@ -0,0 +1,11 @@
+defmodule Google.Cloud.Integrations.V1alpha.IntegrationState do
+  @moduledoc false
+
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :INTEGRATION_STATE_UNSPECIFIED, 0
+  field :DRAFT, 1
+  field :ACTIVE, 2
+  field :ARCHIVED, 3
+  field :SNAPSHOT, 4
+end
