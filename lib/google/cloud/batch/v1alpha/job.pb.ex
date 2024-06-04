aace0a5987db484ca9a3d6f15556adb7094a666a
Hany Rock <rudebono@gmail.com>
2024-06-04 00:29:01 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

--- lib/google/cloud/batch/v1alpha/job.pb.ex
+++ lib/google/cloud/batch/v1alpha/job.pb.ex
@@ -372,6 +372,7 @@ defmodule Google.Cloud.Batch.V1alpha.AllocationPolicy.InstancePolicyOrTemplate d
   field :policy, 1, type: Google.Cloud.Batch.V1alpha.AllocationPolicy.InstancePolicy, oneof: 0
   field :instance_template, 2, type: :string, json_name: "instanceTemplate", oneof: 0
   field :install_gpu_drivers, 3, type: :bool, json_name: "installGpuDrivers"
+  field :install_ops_agent, 4, type: :bool, json_name: "installOpsAgent", deprecated: false
 end
 
 defmodule Google.Cloud.Batch.V1alpha.AllocationPolicy.NetworkInterface do
