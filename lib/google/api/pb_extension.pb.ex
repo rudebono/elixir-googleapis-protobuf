2965327db3c80a291e48a81f79c0e62831f6f849
Hany Rock <rudebono@gmail.com>
2025-01-24 00:33:15 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

--- lib/google/api/pb_extension.pb.ex
+++ lib/google/api/pb_extension.pb.ex
@@ -3,7 +3,17 @@ defmodule Google.Api.PbExtension do
 
   use Protobuf, protoc_gen_elixir_version: "0.14.0"
 
-  extend Google.Protobuf.MethodOptions, :routing, 72_295_729,
+  extend Google.Protobuf.FieldOptions, :resource_reference, 1055,
     optional: true,
-    type: Google.Api.RoutingRule
+    type: Google.Api.ResourceReference,
+    json_name: "resourceReference"
+
+  extend Google.Protobuf.FileOptions, :resource_definition, 1053,
+    repeated: true,
+    type: Google.Api.ResourceDescriptor,
+    json_name: "resourceDefinition"
+
+  extend Google.Protobuf.MessageOptions, :resource, 1053,
+    optional: true,
+    type: Google.Api.ResourceDescriptor
 end
