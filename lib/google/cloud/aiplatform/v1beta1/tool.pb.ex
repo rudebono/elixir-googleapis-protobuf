dec122d4442d53a232144a5c92833bdf2e889275
Hany Rock <rudebono@gmail.com>
2025-03-15 00:33:41 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

--- lib/google/cloud/aiplatform/v1beta1/tool.pb.ex
+++ lib/google/cloud/aiplatform/v1beta1/tool.pb.ex
@@ -241,6 +241,7 @@ defmodule Google.Cloud.Aiplatform.V1beta1.VertexAISearch do
   use Protobuf, protoc_gen_elixir_version: "0.14.1", syntax: :proto3
 
   field :datastore, 1, type: :string, deprecated: false
+  field :engine, 2, type: :string, deprecated: false
 end
 
 defmodule Google.Cloud.Aiplatform.V1beta1.GoogleSearchRetrieval do
