588f9e7d6f7ba05276a8e40d0e4ff537039100e4
Hany Rock <rudebono@gmail.com>
2025-05-17 00:34:58 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

--- lib/google/api/pb_extension.pb.ex
+++ lib/google/api/pb_extension.pb.ex
@@ -3,33 +3,7 @@ defmodule Google.Api.PbExtension do
 
   use Protobuf, protoc_gen_elixir_version: "0.14.1"
 
-  extend Google.Protobuf.EnumOptions, :enum_visibility, 72_295_727,
+  extend Google.Protobuf.MethodOptions, :routing, 72_295_729,
     optional: true,
-    type: Google.Api.VisibilityRule,
-    json_name: "enumVisibility"
-
-  extend Google.Protobuf.EnumValueOptions, :value_visibility, 72_295_727,
-    optional: true,
-    type: Google.Api.VisibilityRule,
-    json_name: "valueVisibility"
-
-  extend Google.Protobuf.FieldOptions, :field_visibility, 72_295_727,
-    optional: true,
-    type: Google.Api.VisibilityRule,
-    json_name: "fieldVisibility"
-
-  extend Google.Protobuf.MessageOptions, :message_visibility, 72_295_727,
-    optional: true,
-    type: Google.Api.VisibilityRule,
-    json_name: "messageVisibility"
-
-  extend Google.Protobuf.MethodOptions, :method_visibility, 72_295_727,
-    optional: true,
-    type: Google.Api.VisibilityRule,
-    json_name: "methodVisibility"
-
-  extend Google.Protobuf.ServiceOptions, :api_visibility, 72_295_727,
-    optional: true,
-    type: Google.Api.VisibilityRule,
-    json_name: "apiVisibility"
+    type: Google.Api.RoutingRule
 end
