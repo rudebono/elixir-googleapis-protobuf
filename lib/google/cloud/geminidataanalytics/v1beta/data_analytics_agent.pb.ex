840346ce9d470662dcba660bb3c1ef3e957e42bf
Hany Rock <rudebono@gmail.com>
2025-07-02 00:36:28 +0000

    AUTO GENERATED ELIXIR GOOGLEAPIS PROTOBUF

---
+++ lib/google/cloud/geminidataanalytics/v1beta/data_analytics_agent.pb.ex
@@ -0,0 +1,20 @@
+defmodule Google.Cloud.Geminidataanalytics.V1beta.DataAnalyticsAgent do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.14.1", syntax: :proto3
+
+  field :staging_context, 5,
+    type: Google.Cloud.Geminidataanalytics.V1beta.Context,
+    json_name: "stagingContext",
+    deprecated: false
+
+  field :published_context, 6,
+    type: Google.Cloud.Geminidataanalytics.V1beta.Context,
+    json_name: "publishedContext",
+    deprecated: false
+
+  field :last_published_context, 7,
+    type: Google.Cloud.Geminidataanalytics.V1beta.Context,
+    json_name: "lastPublishedContext",
+    deprecated: false
+end
